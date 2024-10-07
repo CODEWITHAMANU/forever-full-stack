e1489f3e2616fba80c6b5d4f3c0312217b987c2c
CodeWithAmanu <codewithamanu@gmail.com>
2024-10-07 12:01:12 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '₹';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL"https://forever-full-stack-2.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
