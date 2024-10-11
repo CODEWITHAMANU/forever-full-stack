faec30e847df7507ee7af3e60e6e5b596390ee86
cwamanu7 <codewithamanu7@gmail.com>
2024-10-11 23:06:44 +0530

    final

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const backendUrl = "https://searchbag-backend.vercel.app/"
+export const backendUrl = "https://searchbag-backend.vercel.app"
 export const currency = '₹'
 
 const App = () => {
